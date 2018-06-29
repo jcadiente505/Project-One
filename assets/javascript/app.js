75799ec1737ffa3229f5cb3cb53493a0a3a80607
reggiegfl <reggiegfl@yahoo.com>
2018-06-28 20:51:57 -0400

    added geolocation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -31,3 +31,84 @@ database = firebase.database();
 
 // ------------Functions-----------------//
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Generate coordinates for current location
+var x = document.getElementById("location");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;
+}
